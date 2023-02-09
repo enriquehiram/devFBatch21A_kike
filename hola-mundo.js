7099177c8460572611fa1dfce90c640381b577f3
kikemaya <enrique.mg.cd@gmail.com>
2023-02-08 21:26:22 -0600

    2da semana. asincronismo

--- hola-mundo.js
+++ hola-mundo.js
@@ -1,4 +1,4 @@
-console.log("Hola Mundo, me estoy ejecutando en tiempo real...");
+console.log("Hola Mundo!, me estoy ejecutando en tiempo real...");
 
 // en node js, no existen componentes graficos complejos como alert o prompt
 // alert("Hola, soy una ventana");
