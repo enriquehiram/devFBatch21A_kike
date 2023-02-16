e4f47613b4ec2ffa79de82436ff7e7930f8962fd
kikemaya <enrique.mg.cd@gmail.com>
2023-02-15 23:33:51 -0600

    3era semana. callbacks, timers, cliente - servidor

--- funciones/callback/ejemplo-3.js
+++ funciones/callback/ejemplo-3.js
@@ -1,18 +1,18 @@
-const hola = () => {
-  setTimeout(() => {
-    console.log("Hola");
-  }, 1500);
-};
-
-const adios = () => {
-  return setTimeout(() => {
-    console.log("Adios");
-  }, 1000);
-};
-
-hola();
-adios();
 // ¿CÒMO RESOLVERIAS ESTO CON CALLBACKS?
 // CON AYUDA DE CALLBACKS, CONSIGUE QUE ESTE PROGRAMA SIEMPRE
 // SALUDE PRIMERO, SIN IMPORTAR QUE LA FUNCION "ADIOS", SE
 // EJECUTE ANTES
+const hola = (otraFuncion) => {
+  setTimeout(() => {
+    console.log("Hola");
+    otraFuncion();
+  }, 1500);
+};
+
+const adios = () => {
+  setTimeout(() => {
+    console.log("Adios");
+  }, 1000);
+};
+
+hola(adios);
