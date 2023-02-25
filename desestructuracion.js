0d90a16efb8cebb7d60a6dcdcd82275cd6113244
Enrique De la Cruz <delacruz.g.enrique@gmail.com>
2023-02-25 17:58:38 -0600

    agregando archivos de la ultima clase por mi cuenta

---
+++ desestructuracion.js
@@ -0,0 +1,12 @@
+let auto = {
+    color: "verde",
+    modelo: 2008,
+    marca: "Vocho",
+};
+
+// console.log(auto.color);
+//let colorDeAuto = auto.color;
+
+let {marca, modelo} = auto;
+
+console.log(marca, modelo);
